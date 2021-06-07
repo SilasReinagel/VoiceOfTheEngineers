43d292189fe559b4c51384511b9bfdaffc1c7e8e
Silas Reinagel <silas.reinagel@gmail.com>
2021-06-07 15:59:49 -0700

    UI: Added Home button when not on main view. Added Icons. Closes #19

--- site/src/Layout/SiteHeader.svelte
+++ site/src/Layout/SiteHeader.svelte
@@ -1,6 +1,11 @@
 <script>
   import state, { isLoggedIn } from '../Store';
   import company from '../static-content.js';
+  import HomeIcon from '../Elements/HomeIcon.svelte';
+  import LoginIcon from '../Elements/LoginIcon.svelte';
+  import LogoutIcon from '../Elements/LogoutIcon.svelte';
+
+  const iconButtonSize = 44;
 
   const login = () => state.update(s => ({ ...s, userSession: { id: 1 }}));
   const logout = () => state.update(s => ({ ...s, userSession: {}}));
@@ -8,23 +13,32 @@
 </script>
 
 <header>
-  <button on:click={returnHome} class="logo">
+  <button on:click={returnHome} class="logo transparent-button">
     <div class="logo-img">
       <img src={company.logo} alt="Logo"/>
     </div>
     <h1>{company.name}</h1>
   </button>
 
-  {#if !$isLoggedIn}
-    <button on:click={login}>
-      Login
+  <div class="buttons">
+    {#if $state.currentView !== 'Hottest'}
+    <button on:click={returnHome} class="icon-button">
+      <HomeIcon size={iconButtonSize}/>
     </button>
-  {/if}
-  {#if $isLoggedIn}
-    <button on:click={logout}>
-      Logout
-    </button>
-  {/if}
+    {/if}
+
+    {#if !$isLoggedIn}
+      <button on:click={login} class="icon-button">
+        <LoginIcon size={iconButtonSize}/>
+      </button>
+    {/if}
+    {#if $isLoggedIn}
+      <button on:click={logout} class="icon-button">
+        <LogoutIcon size={iconButtonSize}/>
+      </button>
+    {/if}
+  </div>
+
 </header>
 
 <style>
@@ -34,6 +48,7 @@
     justify-content: space-between;
     align-items: center;
     align-content: center;
+    justify-items: center;
     background-color: var(--header-background-color);
     height: 82px;
     padding: 0 16px;
@@ -50,7 +65,7 @@
     max-height: 92px;
   }
 
-  .logo {
+  .transparent-button {
     background-color: rgba(0,0,0,0);
     border: 0px;
     cursor: pointer;
@@ -60,11 +75,17 @@
     width: 48px;
   }
 
-  button {
+  .icon-button {
     border: 0px;
     border-radius: 6px;
-    background-color: var(--panel-bg);
+    background-color: rgba(0,0,0,0);
     padding: 8px;
-    min-width: 80px;
+    min-width: 48px;
+    min-height: 48px;
+    cursor: pointer;
+    margin-top: auto;
+    margin-bottom: auto;
+    margin-left: 8px;
+    margin-right: 8px;
   }
 </style>
