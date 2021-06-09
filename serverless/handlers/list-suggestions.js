bfdd16c8cc5a32ccbb86092ee30880c7cd204724
Noah Lustig <noah.lustig@prokure1.com>
2021-06-09 09:35:54 -0700

    list-suggestions working

---
+++ serverless/handlers/list-suggestions.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+
+require('dotenv').config(); 
+
+//const { getMongoConfig } = require('../lib/secrets');
+const { Suggestion } = require('../models/suggestion');
+
+let conn = null;
+
+const handler = async function (event, context) {
+    const MongoURI = process.env.MONGO_CONNECTION_STRING; //await getMongoConfig();
+
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    if (conn == null) {
+        mongoose.connect(MongoURI, { useNewUrlParser: true });
+        conn = mongoose.connection
+            .once('open', (conn) => console.log('Connected to Mongo instance.'))
+            .on('error', error => console.log('Error connecting to Mongo:', error));
+    }
+
+    const { authorId } = event;
+
+    const result = await Suggestion.find({authorId});
+
+    console.log('RESULT', result);
+
+    return result;
+}
+
+handler({authorId: 'xyz'}, {});
+
+module.exports.handler = handler;
